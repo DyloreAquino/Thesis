79ea70eec8b4636311463ff195254e1294224cf8
DyloreAquino <lutherjerold@gmail.com>
2026-09-25 12:54:50 +0800

    test: new layout new queueline

--- godot/thesis-godot/journey_entry.gd
+++ godot/thesis-godot/journey_entry.gd
@@ -4,3 +4,4 @@ class_name JourneyEntry
 
 ## Agents spawned here begin at this switch. Empty uses GeometryExporter's initial switch.
 @export var starting_switch: JourneySwitch
+var is_enabled : bool = true
